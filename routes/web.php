d13dd9907b38908b9ab920f4bde98cde36e43af7
Wieke <68997647+Malaila17@users.noreply.github.com>
2025-11-27 10:23:13 +0100

    fixed create view

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
 Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
 Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
 
 
