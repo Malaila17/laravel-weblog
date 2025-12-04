13b601ac815d421d73abb81aa73e08f99ad65057
Wieke <68997647+Malaila17@users.noreply.github.com>
2025-12-04 12:14:16 +0100

    Have to be logged in to place comment

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,3 +1,8 @@
+<?php
+    use Illuminate\Support\Facades\Auth;
+
+    $current_user = Auth::user();
+?>
 @extends('layouts.app')
 
 @section('title', 'Titel van Artikel')
@@ -7,6 +12,7 @@
     <p>{{$article->content}}
     <br>
  
+    @if($current_user)
     <form action="{{ route('comments.store',$article->id) }}" method="POST">
     @csrf
     <label for="content">Nieuwe reactie:</label><br>
@@ -14,6 +20,9 @@
     <br>
     <button type="submit">Opslaan</button>
     </form>
+    @else
+    <p><a href="{{ route('auth.login') }}">Log in om een reactie te plaatsen</a></p>
+    @endif
 
     <h3>Reacties op dit artikel</h3>
     <ul>
